6634c21a0cdfd99dfe945835f60cd926af3b4ace
ivroma <iv.rom96@gmail.com>
2018-07-14 16:53:39 +0300

    Не работает FileProvider

--- app/src/main/java/com/ivrom/camera/MainActivity.java
+++ app/src/main/java/com/ivrom/camera/MainActivity.java
@@ -7,18 +7,23 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.TabHost;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends Activity {
 
     private static final int TAKE_PICTURE_REQUEST = 1;
     private ImageView imageView;
-    private Uri outputFileUri;
+    private String mCurrentPhotoPath;
+    private Bitmap mImageBitmap;
+    private Uri photoURI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,42 +33,54 @@ public class MainActivity extends Activity {
     }
 
     public void onClick(View view) {
-        Toast.makeText(this, "Нажата кнопка", Toast.LENGTH_SHORT).show();
-        getThumbnailPicture();
-        //saveFullImage();
+        Toast.makeText(this, "Нажата кнопка, открываем камеру", Toast.LENGTH_SHORT).show();
+        dispatchTakePictureIntent();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == TAKE_PICTURE_REQUEST && resultCode == RESULT_OK) {
-            // Проверяем, содержит ли результат маленькую картинку
-            if (data != null) {
-                Bitmap thumbnailBitmap = (Bitmap) data.getExtras().get("data");
-                imageView.setImageBitmap(thumbnailBitmap);
-                Toast.makeText(this, "Результат содержит миниатюру", Toast.LENGTH_SHORT).show();
-            }
-            else {
-                // Какие-то действия с полноценным изображением
-                // Сохраняем  по адресу outputFileUri
-                Toast.makeText(this, "Результат содержит полноценное изображение", Toast.LENGTH_SHORT).show();
-                imageView.setImageURI(outputFileUri);
-            }
+        if (requestCode == TAKE_PICTURE_REQUEST && resultCode == RESULT_OK) {
+            imageView.setImageURI(photoURI);
         }
     }
 
-    private void getThumbnailPicture() {
-        Intent intent = new Intent();
-        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, TAKE_PICTURE_REQUEST);
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
     }
 
-    private void saveFullImage(){
-        Intent intent = new Intent();
-        intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-        File file = new File(Environment.getExternalStorageDirectory(),"test.jpg");
-        outputFileUri = Uri.fromFile(file);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT,outputFileUri);
-        startActivityForResult(intent,TAKE_PICTURE_REQUEST);
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Создание файла, в который будет сохраняться фото
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            }
+            catch (IOException exception){
+                Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show();
+            }
+
+            // Продолжаем только если файл успешно создан
+            if (photoFile != null) {
+                photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.provider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, TAKE_PICTURE_REQUEST);
+            }
+
+        }
     }
 }
