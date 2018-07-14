15c58560ba5a57916f0a6fb43e1689169804eeb0
ivroma <iv.rom96@gmail.com>
2018-07-14 21:03:26 +0300

    finished application

--- app/src/main/java/com/ivrom/camera/MainActivity.java
+++ app/src/main/java/com/ivrom/camera/MainActivity.java
@@ -1,81 +1,82 @@
 package com.ivrom.camera;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class MainActivity extends Activity {
 
-    private static final int TAKE_PICTURE_REQUEST = 1;
-    private ImageView imageView;
-    private String mCurrentPhotoPath;
-    private Uri photoURI;
+    final int CAMERA_REQUEST = 1;
+    final int PIC_CROP = 2;
+    private Uri picUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
-        imageView = findViewById(R.id.imageView);
+        setTitle("Съёмка и Кадрирование");
     }
 
-    public void onClick(View view) {
-        Toast.makeText(this, "Нажата кнопка, открываем камеру", Toast.LENGTH_SHORT).show();
-        dispatchTakePictureIntent();
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == TAKE_PICTURE_REQUEST && resultCode == RESULT_OK) {
-            imageView.setImageURI(photoURI);
+    public void onClick(View v) {
+        try {
+            // Намерение для запуска камеры
+            Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(captureIntent, CAMERA_REQUEST);
+        } catch (ActivityNotFoundException e) {
+            // Выводим сообщение об ошибке
+            String errorMessage = "Ваше устройство не поддерживает съемку";
+            Toast toast = Toast
+                    .makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
         }
     }
 
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
-        return image;
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            // Вернулись от приложения Камера
+            if (requestCode == CAMERA_REQUEST) {
+                // Получим Uri снимка
+                picUri = data.getData();
+                // кадрируем его
+                performCrop();
+            }
+            // Вернулись из операции кадрирования
+            else if(requestCode == PIC_CROP){
+                Bundle extras = data.getExtras();
+                // Получим кадрированное изображение
+                Bitmap thePic = extras.getParcelable("data");
+                // передаём его в ImageView
+                ImageView picView = (ImageView)findViewById(R.id.imageView);
+                picView.setImageBitmap(thePic);
+            }
+        }
     }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            // Создание файла, в который будет сохраняться фото
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            }
-            catch (IOException exception){
-                Toast.makeText(this, "Error!", Toast.LENGTH_SHORT).show();
-            }
-
-            // Продолжаем только если файл успешно создан
-            if (photoFile != null) {
-                photoURI = Uri.fromFile(photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, TAKE_PICTURE_REQUEST);
-            }
-
+    private void performCrop(){
+        try {
+            // Намерение для кадрирования. Не все устройства поддерживают его
+            Intent cropIntent = new Intent("com.android.camera.action.CROP");
+            cropIntent.setDataAndType(picUri, "image/*");
+            cropIntent.putExtra("crop", "true");
+            cropIntent.putExtra("aspectX", 1);
+            cropIntent.putExtra("aspectY", 1);
+            cropIntent.putExtra("outputX", 156);
+            cropIntent.putExtra("outputY", 256);
+            cropIntent.putExtra("return-data", true);
+            startActivityForResult(cropIntent, PIC_CROP);
+        }
+        catch(ActivityNotFoundException anfe){
+            String errorMessage = "Извините, но ваше устройство не поддерживает кадрирование";
+            Toast toast = Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT);
+            toast.show();
         }
     }
 }
